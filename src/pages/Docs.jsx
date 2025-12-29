977d2f5368949ecc740b442a54d0a7f8c0880dfa
1zhujianbang <154774553+1zhujianbang@users.noreply.github.com>
2025-12-29 23:56:43 +0800

    page

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -3,7 +3,9 @@ import GraphTaxonomy from '../components/Docs/GraphTaxonomy'
 export default function Docs() {
   return (
     <section className="min-h-screen flex flex-col pt-10 lg:pt-12 pb-12">
-      <GraphTaxonomy />
+      <div className="w-full px-[5px] sm:px-[3px] lg:px-[18px] xl:px-[24px]">
+        <GraphTaxonomy />
+      </div>
     </section>
   )
 }
