c9519da795af0664bb4628529f94f4f4443a318e
黎宴卿 <yi096522251@163.com>
2025-12-31 12:55:42 +0800

    page

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -1,8 +1,11 @@
 import GraphTaxonomy from '../components/Docs/GraphTaxonomy'
+import { useI18n } from '../i18n.jsx'
 
 export default function Docs() {
+  const { t } = useI18n()
   return (
     <section className="min-h-screen flex flex-col pt-24 lg:pt-24 pb-12">
+      <h1 className="sr-only">{t('pages.docsTitle')}</h1>
       <GraphTaxonomy />
     </section>
   )
